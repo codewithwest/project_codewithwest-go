4346b8eb33d09280322cb493283a942239f66bb4
codewithwest <jonaslekgau@gmail.com>
2025-02-12 12:34:53 +0200

    Go graphql server init

---
+++ go.mod
@@ -0,0 +1,17 @@
+module go_server
+
+go 1.22.2
+
+require (
+	github.com/digvijay17july/golang-projects/go-graphql-rest-api v0.0.0-20240111151919-cab69013ac78
+	github.com/graphql-go/handler v0.2.4
+	github.com/rs/zerolog v1.33.0
+)
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/graphql-go/graphql v0.8.1 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
+	golang.org/x/sys v0.12.0 // indirect
+)
---
+++ schema/UserType.go
@@ -0,0 +1,51 @@
+package schema
+
+import (
+	"github.com/digvijay17july/golang-projects/go-graphql-rest-api/repository"
+
+	"github.com/graphql-go/graphql"
+)
+
+func GetUserType() *graphql.Object {
+	userType := graphql.NewObject(
+		graphql.ObjectConfig{
+			Name: "User",
+			Fields: graphql.Fields{
+				"id": &graphql.Field{
+					Type: graphql.Int,
+				},
+				"name": &graphql.Field{
+					Type: graphql.String,
+				},
+				"age": &graphql.Field{
+					Type: graphql.Int,
+				},
+			},
+		},
+	)
+	return userType
+}
+
+/*Defined Schema */
+
+func GetSchema(userType *graphql.Object) (graphql.Schema, error) {
+	schema, err := graphql.NewSchema(graphql.SchemaConfig{
+		Query: graphql.NewObject(graphql.ObjectConfig{
+			Name: "Query",
+			Fields: graphql.Fields{
+				"user": &graphql.Field{
+					Type:        userType,
+					Description: "Get a user by ID",
+					Args: graphql.FieldConfigArgument{
+						"id": &graphql.ArgumentConfig{
+							Type: graphql.NewNonNull(graphql.ID),
+						},
+					},
+					Resolve: repository.GetUser, //method from repository package defined below
+				},
+			},
+		}),
+	})
+
+	return schema, err
+}
