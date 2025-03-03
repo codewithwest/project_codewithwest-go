1d2f04dec798c5602a1188da75383e6f9ab0d189
Jon West <77699400+codewithwest@users.noreply.github.com>
2025-03-03 10:27:06 +0200

    Update handler.go to include origins from .env

--- handler/handler.go
+++ handler/handler.go
@@ -48,8 +48,11 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func enableCors(w *http.ResponseWriter) {
+	allowedOrigins := helper.GetEnvVariable("ALLOWED_ORIGINS")
+        origins := strings.Split(allowedOrigins, ",")
 	header := (*w).Header()
-	header.Add("Access-Control-Allow-Origin", "http://localhost:3002")
+	
+	header.Add("Access-Control-Allow-Origin", origins)
 	header.Add("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
 	header.Add("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With, signature, user_id") // Add "signature"
 	header.Add("Access-Control-Allow-Credentials", "true")
