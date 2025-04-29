6b532638a2dfae585c0bdce469885e53b76d338f
codewithwest <jonaslekgau@gmail.com>
2025-04-29 21:39:25 +0200

    removing unused code

--- schema/schema.go
+++ schema/schema.go
@@ -4,20 +4,16 @@ import (
 	"go_server/schema/admin"
 	"go_server/schema/client"
 	"go_server/schema/projects"
-	"go_server/schema/user"
 	"go_server/types"
 
 	"github.com/graphql-go/graphql"
 )
 
 func GetSchema() (graphql.Schema, error) {
-
 	schema, err := graphql.NewSchema(graphql.SchemaConfig{
 		Query: graphql.NewObject(graphql.ObjectConfig{
 			Name: "Query",
 			Fields: graphql.Fields{
-				"getUser":                    user.GetUserSchema(types.UserType),
-				"getUsers":                   user.GetUsersSchema(types.UserType),
 				"loginAdminUser":             admin.GetLoginAdminUserSchema(types.LoginAdminUserType),
 				"getAdminUsers":              admin.GetAdminUsersSchema(types.AdminUsersQueryType),
 				"getAdminUserAccessRequests": admin.GetAdminUserRequests(types.AdminUserRequestQueryType),
